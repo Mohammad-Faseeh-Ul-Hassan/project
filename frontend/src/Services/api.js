881f9213c0ac6dee6628c8545fe8f4201b6949ad
Mohammad-Faseeh-Ul-Hassan <rathorefaseeh7861@gmail.com>
2023-12-22 12:21:51 +0500

    main

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //  const url= "https://myproject-h7lj.onrender.com"
-const url = "https://localhost:3000/";
+const url = "https://myproject-h7lj.onrender.com";
 
 export const addRecipe = async (recipe) => {
     return await axios.post(`${url}/admin`, recipe);
